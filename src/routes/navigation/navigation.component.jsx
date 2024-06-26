18f81f1a25895ea51ebd3a3ed66c404b78a15ab7
aysenurdev <117058195+aysenurdev@users.noreply.github.com>
2024-06-26 10:52:03 -0700

    navigation-bar added

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,7 +1,7 @@
 import { Link, Outlet } from "react-router-dom";
 import { Fragment } from "react";
-import { ReactComponent as CrwnLogo } from '../../../assets/crown.svg'
-import '../navigation/navigation.styles.scss'
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg'
+import './navigation.styles.scss'
 
 const Navigation = () => {
     return (
@@ -14,6 +14,9 @@ const Navigation = () => {
                <div className="nav-links-container">
                 <Link className="nav-link" to="/shop">
                  SHOP
+                 </Link>
+                <Link className="nav-link" to="/sign-in">
+                 SIGN IN
                  </Link>
                </div>
 
