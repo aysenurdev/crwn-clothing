dee923ee12b4b355b2c8508769705f7a87b3ae94
aysenurdev <117058195+aysenurdev@users.noreply.github.com>
2024-06-29 17:06:17 -0700

    added sign-in with email and password

--- src/component/sign-up-form/sign-up-form.component.jsx
+++ src/component/sign-up-form/sign-up-form.component.jsx
@@ -2,6 +2,9 @@ import SignIn from "../../routes/sign-in/sign-in.component"
 
 import { useState } from "react";
 
+import { createAuthUserWithEmailAndPassword , createUserDocumentFromAuth} from "../../utils/firebase/firebase.utils";
+
+
 
 const defaultFormFields = {
     displayName: '',
@@ -18,6 +21,37 @@ const SignUpForm = () => {
 
   console.log(formFields)
 
+
+const handleSubmit = async (event) => {
+   event.preventDefault();
+
+   if (password !== confirmPassword ){
+    alert("Passwords do not match");
+      return;
+}
+
+try {
+  const{user} = await createAuthUserWithEmailAndPassword( email, password)
+
+
+  await createUserDocumentFromAuth (user, {displayName})
+
+
+
+} catch(error) {
+
+if (error.code = 'auth/email-already-in-use'){
+  alert('Cannot create user, email already in use')
+}else {
+  console.log('user creation encountered an error')
+}
+ 
+
+}
+};
+
+
+
   const handleChange = (event) => {
      const {name, value } = event.target;
 
@@ -29,7 +63,7 @@ const SignUpForm = () => {
 <div>
     <h1>Sign up with your email and password</h1>
 
-    <form onSubmit={() => {}}> 
+    <form onSubmit={handleSubmit}> 
       <label>Display Name</label>
       <input type="text" required onChange={handleChange} name="displayName" value={displayName} />
 
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword } from "firebase/auth";
 
 import {
     getFirestore,
@@ -35,7 +35,10 @@ export const signInWithGoogleRedirect = () => signInWithRedirect(auth,
 
   export const db = getFirestore();
 
-  export const createUserDocumentFromAuth = async (userAuth) => {
+  export const createUserDocumentFromAuth = async (userAuth, additionalInformation = {}) => {
+
+    if (!userAuth) return;
+    
     const userDocRef = doc(db, 'users', userAuth.uid);
 
 
@@ -47,7 +50,7 @@ export const signInWithGoogleRedirect = () => signInWithRedirect(auth,
         const createdAt = new Date();
 
         try {
-            await setDoc(userDocRef, {displayName, email, createdAt});
+            await setDoc(userDocRef, {displayName, email, createdAt, ...additionalInformation});
         } catch (error) {
             console.log('error creating the user' , error.message);
 
@@ -56,4 +59,13 @@ export const signInWithGoogleRedirect = () => signInWithRedirect(auth,
 
      return userDocRef;
 
+  };
+
+
+
+  export const createAuthUserWithEmailAndPassword = async (email, password) => {
+
+    if (!email || !password) return;
+
+    return await  createUserWithEmailAndPassword(auth, email, password)
   };
